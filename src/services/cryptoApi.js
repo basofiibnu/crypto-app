42024fe61f7335d531ca39242b08fbb712ca418e
basofiibnu <>
2022-03-17 03:53:28 +0700

    initialize service api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       tiers: '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'd5dce97f06mshe25723e1fc25a06p1b5a71jsnb2b17703f726'
+//     }
+//   };
