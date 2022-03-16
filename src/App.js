e699f92541c1027d27d45cc0a7efa94f87b59b83
basofiibnu <>
2022-03-17 03:46:19 +0700

    implementing basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Route, Link, Switch, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import "./App.css";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+} from "./components";
 
 const App = () => {
-  return <div>Cryptoverse</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route
+                exact
+                path="/crypto/:coindId"
+                element={<CryptoDetails />}
+              />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
